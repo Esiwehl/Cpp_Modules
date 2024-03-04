ac7b402d2b59308d6b583ced5382d760b0bfc229
Eluse Wehl <ewehl@f1r6s11.codam.nl>
2024-03-04 15:31:18 +0100

    Fixed segf, when more than 8 contacts

--- cpp_00/ex01/Phonebook.cpp
+++ cpp_00/ex01/Phonebook.cpp
@@ -29,7 +29,8 @@ void Phonebook::SEARCH(){
 		int index;
 	
 		std::cout << "Index     |First Name|Last Name |Nickname  " << std::endl;
-		for (int idx = 0; idx < _contactCount; idx++){
+		int max = (_contactCount < 8) ? _contactCount : 8;
+		for (int idx = 0; idx < max; idx++){
 			std::stringstream ss;
 			ss << idx;
 			std::string idxStr = ss.str();
