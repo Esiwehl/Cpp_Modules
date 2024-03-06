caa55cad142941f279f2351a5183e96cdf70bed6
Eluse Wehl <ewehl@f1r6s4.codam.nl>
2024-03-06 11:00:59 +0100

    Fixed the Horde.

--- cpp_01/ex01/zombieHorde.cpp
+++ cpp_01/ex01/zombieHorde.cpp
@@ -1,9 +1,16 @@
 #include "Zombie.hpp"
+#include <iostream>
 
 Zombie* zombieHorde( int N, std::string name ){
+    Zombie *zombieHorde;
     if (N < 0)
-        return NULL;
-    Zombie *zombieHorde = new Zombie[N];
+        return nullptr;
+
+    try{
+        zombieHorde = new Zombie[N];
+    } catch (const std::bad_alloc& e){
+        std::cerr << "Memory allocation failed: " << e.what() << std::endl;
+    }
     for (int idx = 0; idx < N; idx++){
         zombieHorde[idx].setName(name);
     }
