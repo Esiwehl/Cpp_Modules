c60825e618661365f38073f7858478e36939e97b
Eluse Wehl <ewehl@f1r1s6.codam.nl>
2024-03-15 11:07:58 +0100

    Yes, testing es muy importante

--- cpp_02/ex02/src/main.cpp
+++ cpp_02/ex02/src/main.cpp
@@ -1,7 +1,45 @@
 #include <iostream>
 #include "Fixed.hpp"
 
-int main( void ) {
+#define ENDC "\e[0m"
+#define CYAN "\e[36m"
+#define YELLOW "\e[33m"
+#define GREEN "\e[32m"
+
+void arithmetics(){
+	Fixed const a(15);
+	Fixed const b(5);
+	Fixed c;
+
+	std::cout << YELLOW<< "Time for arithmetics:\t" << ENDC << std::endl;
+	c = a + b;
+	std::cout << CYAN << "addition is wonderful:\t" << c << ENDC << std::endl;
+	c = a - b;
+	std::cout << CYAN << "substraction too:\t" << c << ENDC << std::endl;
+	c = a * b;
+	std::cout << CYAN << "How about a multiplication:\t" << c << ENDC << std::endl;
+	c = a / b;
+	std::cout << CYAN << "And some division too:\t" << c << ENDC << std::endl;
+}
+
+void comparison(){
+	Fixed const a(15.54f);
+	Fixed const b(5.54f);
+	Fixed c(a);
+
+	std::cout << "a = " << a << "\t" << "b = " << b << "\t" << "c = " << c << std::endl;
+
+	// c++;
+	// std::cout << "c = " << c << std::endl;
+
+	std::cout << "a < b\t" << ((a < b) ? "True" : "False") << std::endl;
+	std::cout << "a > b\t" << ((a > b) ? "True" : "False") << std::endl;
+
+	std::cout << "a == c\t" << ((a == c) ? "True" : "False") << std::endl;
+	std::cout << "a != c\t" << ((a != c)  ? "True" : "False")<< std::endl;
+}
+
+void subject(){
 	Fixed a;
 	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
 	
@@ -15,6 +53,14 @@ int main( void ) {
 	std::cout << b << std::endl;
 	
 	std::cout << Fixed::max( a, b ) << std::endl;
-	
+	std::cout << Fixed::min( a, b ) << std::endl;
+}
+
+int main() {
+
+	// subject();
+	// comparison();
+	// arithmetics();
+
 	return 0;
 }
