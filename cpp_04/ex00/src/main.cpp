e11ab239b7aefb9943e3a04f0c1b8f0066374a54
Eluse Wehl <ewehl@f1r5s7.codam.nl>
2024-03-22 13:38:20 +0100

    More complete testing

--- cpp_04/ex00/src/main.cpp
+++ cpp_04/ex00/src/main.cpp
@@ -6,9 +6,12 @@
 #include "WrongCat.hpp"
 
 int main() {
+    std::cout << "\n\t[ CONSTRUCTORS CALLS ]\n" << std::endl;
 	const Animal* genericAnimal = new Animal();
 	const Animal* dog = new Dog();
 	const Animal* cat = new Cat();
+
+    std::cout << "\n---------------------------------------\n" << std::endl;
 
 	std::cout << dog->getType() << ": ";
 	dog->makeSound();
@@ -19,20 +22,35 @@ int main() {
 	std::cout << genericAnimal->getType() << ": ";
 	genericAnimal->makeSound();
 
+    std::cout << "\n\t[ DESTRUCTOR CALLS ]\n" << std::endl;
+
+    delete genericAnimal;
+	delete dog;
+	delete cat;
+
+    std::cout << "\n---------------------------------------\n" << std::endl;
+
 	const WrongAnimal* wrongAnimal = new WrongAnimal();
 	const WrongAnimal* wrongCat = new WrongCat();
+    const WrongCat* wrongCat2 = new WrongCat();
+
+    std::cout << "\n---------------------------------------\n" << std::endl;
 
 	std::cout << wrongCat->getType() << ": ";
 	wrongCat->makeSound();
 
+    std::cout << "\n\t[What it should've been ]\n" << std::endl;
+    std::cout << wrongCat->getType() << ": ";
+	wrongCat2->makeSound();
+    std::cout << std::endl;
+
 	std::cout << wrongAnimal->getType() << ": ";
 	wrongAnimal->makeSound();
 
-	delete genericAnimal;
-	delete dog;
-	delete cat;
+    std::cout << "\n\t[ DESTRUCTOR CALLS ]\n" << std::endl;
 	delete wrongAnimal;
 	delete wrongCat;
+    delete wrongCat2;
 
 	return 0;
 }
