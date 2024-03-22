0196e74c0df18e5ac51b78e76bbd5ceee99595c1
Eluse Wehl <ewehl@f1r5s7.codam.nl>
2024-03-22 14:06:48 +0100

    Made brain, or life should I say/

---
+++ cpp_04/ex01/inc/Brain.hpp
@@ -0,0 +1,24 @@
+#ifndef BRAIN_HPP
+# define BRAIN_HPP
+
+# include <iostream>
+# include <string>
+
+class Brain {
+    private:
+        std::string[100] ideas;
+    public:
+        Brain(/* args */);
+        ~Brain();
+};
+
+Brain::Brain(/* args */)
+{
+}
+
+Brain::~Brain()
+{
+}
+
+
+#endif
